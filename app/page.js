fad378a06ea6167d25f864fe0fbb3b57a3643c64
IVAN <howls.sstudio@gmail.com>
2026-04-26 15:44:04 +0700

    Thay đổi 'INITIATE_HACK' thành 'IVAN'

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function HomePage() {
   const [systemStatus, setSystemStatus] = useState("SYS.ERROR // BREACH DETECTED");
   const [systemStatusVisible, setSystemStatusVisible] = useState(false);
   const [systemStatusLocked, setSystemStatusLocked] = useState(false);
-  const [drawButtonLabel, setDrawButtonLabel] = useState("> INITIATE_HACK");
+  const [drawButtonLabel, setDrawButtonLabel] = useState("> IVAN");
   const [customTextShadow, setCustomTextShadow] = useState("");
 
   const canvasRef = useRef(null);
@@ -141,7 +141,7 @@ export default function HomePage() {
     setResultClass("");
     setSystemStatusVisible(false);
     setSystemStatusLocked(false);
-    setDrawButtonLabel("> INITIATE_HACK");
+    setDrawButtonLabel("> IVAN");
     setCustomTextShadow("");
     setState("standby");
     matrixSpeedRef.current = 1;
